19a26c4a48da58c1a1e758d8b52fcd4611f83ad9
ArjunB126 <arjunbajare86@gmail.com>
2024-02-27 20:14:19 +0530

    byte code datatype practise

---
+++ Byte_Datatype/QspiderStudentsRatings.java
@@ -0,0 +1,23 @@
+class QspiderStudentsRatings
+{
+
+	public static void main (String [] args)
+   {
+		byte super_Star = 5;
+		byte one_Star = 10;
+		byte two_Star = 53;
+		byte three_Star = 100;
+		byte poorPerformance = 126;
+
+		
+		System.out.println("Number Of Star Performers : " + super_Star );
+		System.out.println("Number Of One Star Performers : " + one_Star  );
+		System.out.println("Number Of Two Star Performers : " + two_Star );
+		System.out.println("Number Of Three Star Performers : " + three_Star );
+		System.out.println("Number Of Four Star Performers : " + poorPerformance );
+
+
+
+   }
+
+}
