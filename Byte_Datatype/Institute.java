1280f5c1097c116ea4938a2b5d178f59495bc1a0
ArjunB126 <arjunbajare86@gmail.com>
2024-02-27 22:35:48 +0530

    byte code datatype practise

---
+++ Byte_Datatype/Institute.java
@@ -0,0 +1,20 @@
+class Institute {
+
+public static void main (String [] args)
+{
+
+	byte noFloors = 8;
+	byte staff = 20;
+	byte lifts = 4;
+	byte subjects = 10;
+	byte batches = 20;
+	byte noRooms = 15;
+
+	System.out.println("No of Rooms -" +  noRooms);
+	System.out.println("No of staff -" + staff);
+	System.out.println("No of batches -" + batches);
+	System.out.println("No of Floors -" + noFloors);
+
+}
+
+}
