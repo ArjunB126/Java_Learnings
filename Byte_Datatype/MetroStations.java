a136dbefb774315521db0dd5e7452f8ee86af726
ArjunB126 <arjunbajare86@gmail.com>
2024-02-28 20:57:09 +0530

    byte Practise MetroStations  Code

---
+++ Byte_Datatype/MetroStations.java
@@ -0,0 +1,15 @@
+class MetroStations {
+    public static void main(String[] args) {
+        byte puneMetroStations = 40;
+        byte mumbaiMetroStations = 20;
+        byte jaipurMetroStations = 50;
+        byte delhiMetroStations = 100;
+        byte chennaiMetroStations = 60;
+
+        System.out.println("Number of Metro Stations in Pune: " + puneMetroStations);
+        System.out.println("Number of Metro Stations in Mumbai: " + mumbaiMetroStations);
+        System.out.println("Number of Metro Stations in Jaipur: " + jaipurMetroStations);
+        System.out.println("Number of Metro Stations in Delhi: " + delhiMetroStations);
+        System.out.println("Number of Metro Stations in Chennai: " + chennaiMetroStations);
+    }
+}
