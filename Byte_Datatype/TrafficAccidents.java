4428b18ed80c9619340768ebd872b53ddd51f686
ArjunB126 <arjunbajare86@gmail.com>
2024-02-28 20:55:32 +0530

    byte practise TrafficAccidents Code

---
+++ Byte_Datatype/TrafficAccidents.java
@@ -0,0 +1,15 @@
+class TrafficAccidents {
+    public static void main(String[] args) {
+        byte fatal = 5;
+        byte severe = 15;
+        byte moderate = 30;
+        byte minor = 50;
+        byte noInjury = 70;
+
+        System.out.println("Number of Fatal Accidents: " + fatal);
+        System.out.println("Number of Severe Accidents: " + severe);
+        System.out.println("Number of Moderate Accidents: " + moderate);
+        System.out.println("Number of Minor Accidents: " + minor);
+        System.out.println("Number of Accidents with No Injury: " + noInjury);
+    }
+}
