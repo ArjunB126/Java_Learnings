61d4d84dcc77055a3f3a7fc5354fbf92f7bda421
ArjunB126 <arjunbajare86@gmail.com>
2024-02-29 20:12:09 +0530

    long datatype practise Github Repositories code

---
+++ long_Datatype/GithubRepositories.java
@@ -0,0 +1,21 @@
+class GithubRepositories
+{
+	public static void main (String [] args)
+	{
+		long react_Repositories = 23456789011234l;
+		long angular_Repositories = 9876567845l;
+		long js_Repositories = 234986785987543l;
+		long css_Repositories = 876543219876578l;
+		long java_Repositories = 9084567891011l;
+
+		System.out.println("Total Number of React Related Repositories On Github : " + " " + react_Repositories);
+		System.out.println("Total Number of Angular Related Repositories On Github : " + " " + angular_Repositories);
+		System.out.println("Total Number of Java-Script [Js] Related Repositories On Github : " + " " +js_Repositories);
+		System.out.println("Total Number of Css Related Repositories On Github : " + " " +  css_Repositories);
+		System.out.println("Total Number of Java Related Repositories On Github : " + " " + java_Repositories);
+	}
+
+
+
+
+}
