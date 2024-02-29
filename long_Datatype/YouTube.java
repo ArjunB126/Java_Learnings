1f54f8e1e1d7197af09bba4db4f9194a86e3ccc4
ArjunB126 <arjunbajare86@gmail.com>
2024-02-29 19:59:39 +0530

    long datatype practise codes

---
+++ long_Datatype/YouTube.java
@@ -0,0 +1,22 @@
+class YouTube
+ {
+
+	public static void main ( String [] args )
+
+	{
+		long techVideos = 2345678910l;
+		long comedyVideos = 786543215l;
+		long scientificVideos = 7654321986l;
+		long millionViewsVideos = 34567892345l;
+		long movieVideos = 2345678987653456l;
+
+
+		System.out.println("Total Tech Videos on Youtube : " + " " + techVideos );
+		System.out.println("Total Comedy Videos on Youtube : " + " " + comedyVideos );
+		System.out.println("Total Scientific Videos on Youtube : " + " " + scientificVideos );
+		System.out.println("Total Millon Views Videos on Youtube : " + " " + millionViewsVideos);
+		System.out.println("Total Movie Videos on Youtube : " + " " + movieVideos );
+		
+	}
+
+}
