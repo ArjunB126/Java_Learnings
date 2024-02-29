c400c962c797f63816ccdc1c5ab53047580dcdba
ArjunB126 <arjunbajare86@gmail.com>
2024-02-29 19:39:13 +0530

    int supply chain code

---
+++ int_Datatype/SupplyChain.java
@@ -0,0 +1,20 @@
+class SupplyChain
+ {
+
+	public static void main ( String [] args )
+	{
+		long numberOfuniqueTrackingCode = 342167832782l;
+		long numberOfCustomersInState = 124563784l;
+		long numberOfReapeatCustomers = 678954321234l;
+		long numberOfWholesalers = 45625625742672676l;
+		long numberOfRetailers = 5466446341238l;
+
+		System.out.println("Companies Total Unique Tracking Ids :" + numberOfuniqueTrackingCode );
+		System.out.println("Companies Total Customers in State :" + numberOfCustomersInState );
+		System.out.println("Companies Total Repeat Customers :" + numberOfReapeatCustomers);
+		System.out.println("Companies Total Whole Sellers :" + numberOfWholesalers );
+		System.out.println("Companies Total Retailers :" + numberOfRetailers);
+		
+	}
+
+}
