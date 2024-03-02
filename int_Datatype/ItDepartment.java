ae9e4872603dcd8f37ed18d207c16770169a9f0e
ArjunB126 <arjunbajare86@gmail.com>
2024-03-02 23:08:16 +0530

    int datatype practise code

---
+++ int_Datatype/ItDepartment.java
@@ -0,0 +1,17 @@
+class ItDepartment 
+
+{
+	public static void main (String [] args )
+	{
+		int totalStudents = 560;
+		int totalStaff = 43;
+		int curriculamSubjects = 50;
+		int seminarHallCapacity = 5000;
+
+	System.out.println("Total Number of Students in IT Department : " + " " + totalStudents  );
+	System.out.println("Total Number of Staff in IT Department : " + " " + totalStaff );
+	System.out.println("Total Number of Subjects in IT Department : " + " " + curriculamSubjects );
+	System.out.println("Total Capacity of Seminar Hall of IT Department : " + " " + seminarHallCapacity );
+
+	}
+}
