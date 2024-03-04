e634621b5742fd08a39e6d27d020ec8b43f6c4e4
ArjunB126 <arjunbajare86@gmail.com>
2024-03-04 10:09:08 +0530

    float Datatype practise

---
+++ Float_Datatype/BankBalance.java
@@ -0,0 +1,20 @@
+
+
+class BankBalance
+{
+	public static void main ( String [] args)
+	{
+		float savings = 54.3435758167f;
+		float stocks = 4554.4641f;
+		float mutualFunds = 34.645789f;
+		float fixDeposits = 3451315.77261f;
+
+		System.out.println("Total Savings Balance : "+ " "+  savings);
+		System.out.println("Total Stocks  Balance: "+ " "+  stocks);
+		System.out.println("Total Mutual Funds Balance: "+ " "+  mutualFunds);
+		System.out.println("Total Fix Deposit Balance : "+ " "+  fixDeposits);
+
+	}
+
+
+}
