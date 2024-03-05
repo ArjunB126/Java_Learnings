1929a9ebed8350433f444fbb360053ef1a0b56b5
ArjunB126 <arjunbajare86@gmail.com>
2024-03-05 10:09:08 +0530

    All  Datatypes practise

---
+++ Assignment_All_Datatype/IplMatch.java
@@ -0,0 +1,39 @@
+class IplMatch   
+
+{
+	public static void main( String []  args )
+	{
+
+		byte team_1_Size = 25;
+		byte team_2_Size = 25;
+		short highestRunsScored = 644;
+		short indivisual_Highest_Runs = 15684;
+		int stadium_Capacity = 80000;
+		int total_Tickets_Sold = 756984;
+		long money_SpendIn_Auction = 896542374589l;
+		long team_Expenditures_Of_Season = 6541534864146854l;
+
+		System.out.println(" Squad of Team 1 "+ " "+ team_1_Size);
+
+		System.out.println(" Squad of Team 2 "+ " "+ team_2_Size);
+
+		System.out.println(" Highest Runs Scored By Team "+ " "+ highestRunsScored);
+
+		System.out.println(" Highest Indivisual Runs  "+ " "+ indivisual_Highest_Runs );
+				
+		System.out.println(" Total Seating Capacity of Stadium "+ " "+stadium_Capacity );
+
+		System.out.println(" Total Tickets Sold For The Match  "+ " "+total_Tickets_Sold);
+
+		System.out.println(" Total Money Spend in Auction"+ " "+ money_SpendIn_Auction);
+
+		System.out.println(" Teams Total Expenditure for One Season "+ " "+ team_Expenditures_Of_Season);
+
+
+
+	}
+
+
+
+
+}
