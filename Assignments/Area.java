1f28d69534ba6aea08fe4fa234961d72a821df89
ArjunB126 <arjunbajare86@gmail.com>
2024-03-06 23:12:31 +0530

    Assignment Codes

---
+++ Assignments/Area.java
@@ -0,0 +1,19 @@
+class Area { 
+
+public static void main (String [] args)
+{
+
+	float radius = 5.5f;
+	float pi = 3.14f;
+
+	float circle_Perimeter = 2 * pi * radius; // 2pi r
+	float circle_Area = pi * (radius * radius); // pi*radius*radius
+
+
+	System.out.println("The Area Of Circle is :" + " " + circle_Area);
+	System.out.println("The Area Of Circle is :" + " " + circle_Perimeter);
+}
+
+
+
+}
