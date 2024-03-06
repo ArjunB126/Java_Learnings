c9c558a0f9beb57281ad52af220e075d91aa3c7c
ArjunB126 <arjunbajare86@gmail.com>
2024-03-06 23:25:37 +0530

    Area Of Rectangle code

---
+++ Assignments/AreaOfRectangle.java
@@ -0,0 +1,25 @@
+class  AreaOfRectangle 
+
+{
+
+
+public static void main ( String [] args ){
+
+
+	float width = 4.5f;
+	float height = 7.9f;
+	float areaOfRectangle = (width*height);
+	float perimeterOfRectangle = (width*height)*2;
+
+
+	System.out.println("The Area of Rectangke is " + " "+ areaOfRectangle);	System.out.println("The Perimeter of Rectangke is " + " "+ perimeterOfRectangle
+);
+
+}
+
+
+
+
+
+
+}
