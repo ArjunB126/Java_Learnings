9149a8acedfa967a9e51826b5bcc3b98d08632eb
ArjunB126 <arjunbajare86@gmail.com>
2024-03-10 23:36:24 +0530

    Area of Circle

---
+++ Assignments/AreaOfCircle.java
@@ -0,0 +1,16 @@
+// Write a java program to find the area of a circle  whose radius is 2.5 cm ( pie r sq)
+
+
+class AreaOfCircle
+
+{
+
+	public static void main ( String [] args )
+	{
+		double radius = 2.5;
+		final double pi = 3.14;
+		double area = pi*(radius*radius);
+
+		System.out.println("The Area of Circle is :" + " " + area +" " + "cm^2");
+	}
+}
