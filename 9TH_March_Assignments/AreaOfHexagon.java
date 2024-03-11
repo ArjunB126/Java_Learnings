502c18d2721ae71e5ac1769638075a2037a3ca94
ArjunB126 <arjunbajare86@gmail.com>
2024-03-11 13:43:40 +0530

    Area of Hexagon Code Updates

---
+++ 9TH_March_Assignments/AreaOfHexagon.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class AreaOfHexagon
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the One Side of Hexagon :");
+		double side = sc.nextDouble();
+
+		double area = 3*((2*2)/2)*(side * side);
+		System.out.println("The Area of Hexagon Is :"+ area);
+	}
+}
