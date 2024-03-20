d91f5fff50eca447de59b8b2facb44eacbbb1eaa
ArjunB126 <arjunbajare86@gmail.com>
2024-03-20 23:20:45 +0530

    Loops reverse

---
+++ Loops/LoopsExRev.java
@@ -0,0 +1,22 @@
+class LoopsExRev{
+	public static void main(String[] args) {
+
+
+		for (char ch = 'Z'; ch >= 'A' ; ch -- ) {
+			System.out.print(ch + " ");
+		}
+
+		
+
+		for (char ch = 122; ch>=97 ; ch -- ) {
+			System.out.print(ch + " ");
+		}
+
+		for (char ch = '9'; ch>= 48 ;ch -- ) {
+			System.out.println(ch + " ");
+			
+		}
+
+
+	}
+}
