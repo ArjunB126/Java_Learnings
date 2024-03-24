02a2b86429d9ec8439ea848a2217ce94991031b9
ArjunB126 <arjunbajare86@gmail.com>
2024-03-24 22:58:10 +0530

    Number to Word Conversion

--- Loops/NumberToWord.java
+++ Loops/NumberToWord.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class NumberToWord {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // No Need To Close The Scanner
         System.out.println("Enter a Number");
         int number = sc.nextInt();
         String word = "";
