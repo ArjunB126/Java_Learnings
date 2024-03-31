7969729864ddf4f4f707eb8d69170f2db13a921d
ArjunB126 <arjunbajare86@gmail.com>
2024-03-31 10:15:23 +0530

    Labeled Loops java

---
+++ Loops/LabelExample.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class LabelExample{
+	public static void main(String[] args) {
+		ram:{
+		int i = 25;
+		while(i>=20){
+			System.out.println(i+" ");
+			if (i==22) {
+				break ram;
+			  }
+		 }
+	   }
+	}
+}
