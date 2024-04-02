07c07027e389d52f2b2d4862e9e59f63224bbfdb
ArjunB126 <arjunbajare86@gmail.com>
2024-04-02 10:15:23 +0530

    Methods in java

---
+++ _Methods_/Example100.java
@@ -0,0 +1,22 @@
+class Example100{
+
+	public static void main(String[] args) {
+		demo('A');
+	}
+
+	public static void demo(byte a){
+		System.out.println("Im Byte");
+	}
+
+    public static void demo( short a){
+		System.out.println("Im Short");
+	}
+
+	public static void demo(int  a){
+		System.out.println("Im Int");
+	}
+
+	public static void demo( char a){
+		System.out.println("Im Char");
+	}
+}
