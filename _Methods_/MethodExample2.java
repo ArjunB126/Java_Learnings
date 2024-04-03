fa0563f60687148c9e98b9230163742e7374bc74
ArjunB126 <arjunbajare86@gmail.com>
2024-04-03 23:16:14 +0530

    Methods in java

---
+++ _Methods_/MethodExample2.java
@@ -0,0 +1,16 @@
+class MethodExample2{
+	public static void main(String[] args) {
+		
+	for (int i = 2 ; i < 50 ; i++ ) {
+		evenOdd(i);
+	}
+}
+	public static void evenOdd(int num){
+		if (num%2 == 0) {
+			System.out.println(num + " Is a Even Number");
+		}else{
+			System.out.println(num + " Is Not a Even Number");
+		}
+	} 
+	
+}
